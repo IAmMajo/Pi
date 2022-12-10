cbf9665a35f59ba33da36a1456898f74c431f536
Majo <git@maxoverlack.dev>
2022-12-10 18:51:25 +0100

    Add error handling

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,22 @@ import "@loadingio/css-spinner/entries/ring/index.css";
 import "./style.css";
 
 const decimalPlaces = document.getElementById("decimal-places")!;
-new Worker(new URL("worker.ts", import.meta.url), {
+const worker = new Worker(new URL("worker.ts", import.meta.url), {
   type: "module",
-}).addEventListener("message", (event: MessageEvent<string>) => {
+});
+worker.addEventListener("message", (event: MessageEvent<string>) => {
   const span = document.createElement("span");
   span.innerText = event.data;
   decimalPlaces.append(span);
+});
+worker.addEventListener("error", () => {
+  const message = document.createElement("p");
+  message.classList.add("error-text");
+  message.textContent =
+    "Ihr Browser kann mit der aktuell zur Berechnung von Pi verwendeten " +
+    "Methode keine weiteren Nachkommastellen berechnen. Benutzen Sie einen " +
+    "Chromium-basierten Browser (beispielsweise die nicht-iOS-Versionen von " +
+    "Chrome, Edge oder Opera), um am meisten Nachkommastellen berechnen zu " +
+    "können.";
+  document.querySelector(".lds-ring")!.replaceWith(message);
 });
